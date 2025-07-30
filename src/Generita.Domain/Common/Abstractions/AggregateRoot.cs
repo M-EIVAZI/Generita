35a9083e77b94d7962f4139a6a092ec9ec9700a1
MAHDI-EIVAZI <mahdieivazi.1397@gmail.com>
2025-07-30 15:59:53 +0330

    Adding Domain Abstractions

---
+++ src/Generita.Domain/Common/Abstractions/AggregateRoot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generita.Domain.Common.Abstractions
+{
+    public  class AggregateRoot:BaseEntity
+    {
+    }
+}
---
+++ src/Generita.Domain/Common/Abstractions/BaseEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Generita.Domain.Models;
+
+namespace Generita.Domain.Common.Abstractions
+{
+    public class BaseEntity
+    {
+        protected Guid _id;
+        protected BaseEntity(Guid Id)
+        {
+            this._id = Id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            return ((Entity)obj).Id == _id;
+        }
+
+        public override int GetHashCode()
+        {
+            return _id.GetHashCode();
+        }
+
+        protected BaseEntity() { }
+    }
+}
